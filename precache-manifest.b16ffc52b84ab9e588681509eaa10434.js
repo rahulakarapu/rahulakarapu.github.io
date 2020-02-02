4d0dbb2e42266e0bf25db322dab0098a3fa67161
rahulakarapu <rahul.akarapu06@gmail.com>
2020-02-02 14:54:40 +0530

    Updates

---
+++ precache-manifest.b16ffc52b84ab9e588681509eaa10434.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e417f65d199173e1bb4e5691c5a0b8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d2b0627660debf56365",
+    "url": "/static/css/main.cc491600.chunk.css"
+  },
+  {
+    "revision": "cc8da89df83281ca27e3",
+    "url": "/static/js/2.b20171b8.chunk.js"
+  },
+  {
+    "revision": "8d2b0627660debf56365",
+    "url": "/static/js/main.915f010e.chunk.js"
+  },
+  {
+    "revision": "638daaa464e5a04d90fc",
+    "url": "/static/js/runtime~main.83e5e4c8.js"
+  },
+  {
+    "revision": "d33e2d9b162771a43a6914fbf6acffab",
+    "url": "/static/media/ListReport.d33e2d9b.jpg"
+  },
+  {
+    "revision": "8589f555c420cbf2e0621baa625dd918",
+    "url": "/static/media/ObjectPage.8589f555.jpg"
+  },
+  {
+    "revision": "59410ce0e5c54ce283da0c8b4b4f677b",
+    "url": "/static/media/OverviewPage.59410ce0.jpg"
+  },
+  {
+    "revision": "c61857c3cfef02aef5a87fc258f3c3c0",
+    "url": "/static/media/PersonalCrop.c61857c3.JPG"
+  },
+  {
+    "revision": "4a171d776e6e0710e8c5690bdc582d68",
+    "url": "/static/media/Worklist.4a171d77.jpg"
+  },
+  {
+    "revision": "b4b987f5983c612b8afa6cb73752e50f",
+    "url": "/static/media/code1.b4b987f5.gif"
+  },
+  {
+    "revision": "f988b6aacaf39b867b8be54fd5d409a3",
+    "url": "/static/media/theHolyKitchen.f988b6aa.jpg"
+  },
+  {
+    "revision": "76593e5a19c3942bf726bfc5ca35528f",
+    "url": "/static/media/theOfficeIpsum.76593e5a.jpg"
+  }
+]);
